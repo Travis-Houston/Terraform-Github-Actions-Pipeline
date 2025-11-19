e928e7ebe1cad0f77aed9c1d71f7ba7c6a3b75c2
Travis-Houston <baohnq2003@gmail.com>
2025-11-19 19:31:30 +0700

    fixing set-output problem and test Tf cloud

--- Student-Management/backend.tf
+++ Student-Management/backend.tf
@@ -1,15 +1,6 @@
 terraform { 
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.2"
-    }
-  }
-
-# Below block is used to specify backed as terraform cloud with organization and workspacename
-# feel free to change as per your environment.
-
   cloud { 
+    
     organization = "Terraform-Labs-2025" 
 
     workspaces { 
