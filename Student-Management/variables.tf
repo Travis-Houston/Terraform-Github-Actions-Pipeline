2df6630ee5edeae2751289b6dd5e15895595675c
Travis-Houston <baohnq2003@gmail.com>
2025-11-19 18:47:00 +0700

    init commit

--- Student-Management/variables.tf
+++ Student-Management/variables.tf
@@ -1,25 +1,31 @@
-variable "key-name" {
-    description = "Name of an existing EC2 KeyPair to enable SSH access to the instance"
-}
+# variable "key-name" {
+#     description = "Name of an existing EC2 KeyPair to enable SSH access to the instance"
+# }
 
-variable "network-security-group-name" {
-    description = "Name of an existing EC2 Security Group to associate with the instance"
-}
+# variable "network-security-group-name" {
+#     description = "Name of an existing EC2 Security Group to associate with the instance"
+# }
 
-variable "ubuntu-ami" {
-    description = "The AMI ID of the Ubuntu image to use for the instance"
-}
+# variable "ubuntu-ami" {
+#     description = "The AMI ID of the Ubuntu image to use for the instance"
+# }
 
-variable "ubuntu-instance-type" {
-    description = "The type of instance to start"
-}
+# variable "ubuntu-instance-type" {
+#     description = "The type of instance to start"
+# }
 
-variable "access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
+# variable "access_key" {
+#   description = "AWS Access Key"
+#   type        = string
+# }
 
-variable "secret_key" {
-  description = "AWS Secret Key"
-  type        = string
+# variable "secret_key" {
+#   description = "AWS Secret Key"
+#   type        = string
+# }
+
+#variables.tf
+
+variable "displaymsg" {
+    default = "Hello I am a null resource"
 }
